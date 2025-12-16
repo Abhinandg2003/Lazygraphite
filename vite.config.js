a89930bd4f29bced3cfa7f58c9f435f9cfdb9127
Abhinandg2003 <abhinandg2003@gmail.com>
2025-12-16 15:52:47 +0530

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio/',
+  base: '/Lazygraphite/',
   plugins: [react(),tailwindcss()],
   resolve:{
     alias:{
