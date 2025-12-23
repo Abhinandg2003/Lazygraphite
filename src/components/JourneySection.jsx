44ec35c0abea7c671fb8cd07a831fed726a7a640
Abhinandg2003 <abhinandg2003@gmail.com>
2025-12-23 16:17:35 +0530

    icon2

--- src/components/JourneySection.jsx
+++ src/components/JourneySection.jsx
@@ -6,7 +6,7 @@ const journeyTimeline = [
     {
         type: "text",
         content:
-            "I didn’t start out knowing what I wanted to become. The journey was full of confusion and self doubt. Yeah, I was the 'Art Kid' ever since I remember. But, I think everybody loved art when they were kids without even knowing what that is. But as I grew I knew i realised that I was more than just an 'Art kid', I was living for it.🌱"
+            "I didn’t start out knowing what I wanted to become. The journey was full of confusion and self doubt. Yeah, I was the 'Art Kid' ever since I remember. But, I think everybody loved art when they were kids without even knowing what that is. But as I grew I realised that I was more than just an 'Art kid', I was living for it.🌱"
     },
     {
         type: "media",
@@ -46,7 +46,7 @@ const journeyTimeline = [
     {
         type: "text",
         content:
-            "This was the stage I was really pulled into art, I was like 'I have to learn this'. I Bought a basic pen tablet (Huion HS64, I still use this😅). I was using my brother's old laptop. It was so laggy in Photoshop, so i had to use Clip Studio Paint. I was really amused by other great artists' works, and then self doubt came in. I was so doubtful of myself that i didn't know if i could do good. I stood Inside my comfort zone. I traced images and tried stylizing without even knowing the fundamentals. I did create art, but I  wasn't really satisfied with what I created. I still posted it on Instagram and showed my friends. My friends and family really encouraged me. Even though there were downs, with my people's motivation, I continued creating.🐥"
+            "This was the stage I really got pulled into art, I was like 'I have to learn this'. I Bought a basic pen tablet (Huion HS64, I still use this😅). I was using my brother's old laptop. It was so laggy in Photoshop, so I had to use Clip Studio Paint. I was really amused by other great artists' works, and then self doubt came in. I was so doubtful of myself that i didn't know if I could do good. I stood Inside my comfort zone. I traced images and tried stylizing without even knowing the fundamentals. I did create art, but I  wasn't really satisfied with what I created. I still posted it on Instagram and showed my friends. My friends and family really encouraged me. Even though there were downs, with my people's motivation, I continued creating.🐥"
     },
 
     {
@@ -65,7 +65,7 @@ const journeyTimeline = [
     {
         type: "text",
         content:
-            "After becoming tired with broken stylisations and boring tracings, I finally started to learn the fundamentals. After a bunch of YouTube tutorials, books, inspirations and a lot of patience and practice, I started to make my fundamentals strong. I Started learning about the facial planes, the human anatomy, line weight, color theory, compositions and everything. I bought a new laptop and switched to Photoshop from Clip Studio Paint. I learned a lot from YouTubers like Angel Ganev, Proko, Samdoesarts etc. as well as from the book 'Drawing the head and hands' by Andrew Loomis. But I still had a lot to learn. I tried stylisations but it was still broken.🍃"
+            "After becoming tired with broken stylisations and boring tracings, I finally started to learn the fundamentals. After a bunch of YouTube tutorials, books, inspirations and a lot of patience and practice, I started to make my fundamentals strong. I Started learning about the facial planes, the human anatomy, line weight, color theory, compositions and everything. I bought a new laptop and switched to Photoshop from Clip Studio Paint. I learned a lot from YouTubers like Angel Ganev, Proko, Samdoesarts etc as well as from the book 'Drawing the head and hands' by Andrew Loomis. But I still had a lot to learn. I tried stylisations but it was still broken.🍃"
     },
 
     {
@@ -84,7 +84,7 @@ const journeyTimeline = [
     {
         type: "text",
         content:
-            "Although I was so focused on learning, art was always an escape from my real life and responsibilities. It was at this time, I really learned about stylizations, that stylization is something that distinguishes one's art from others. Mainly inspired from Samdoesarts I started to work on my stylizations more. Apart from that, I started to incorporate my culture (South Indian) into my artworks mainly by sarees. And that really helped with my motivation to go further, because when I started incorporating this culture I started getting more likes on Instagram. Then I started drawing more sarees😅. It was at that time I realised I could do things. But I still thought i deserved more and I was not getting it, so self doubt followed me everywhere.🌾"
+            "Although I was so focused on learning, art was always an escape from my real life and responsibilities. It was at this time, I really learned about stylizations, that stylization is something that distinguishes one's art from others. Mainly inspired from 'Samdoesarts' I started to work on my stylizations more. Apart from that, I started to incorporate my culture (South Indian) into my artworks mainly by sarees. And that really helped with my motivation to go further, because when I started incorporating this culture I started getting more likes on Instagram. Then I started drawing more sarees😅. It was at that time I realised I could do things. But I still thought I deserved more than what I was getting, so self doubt followed me everywhere.🌾"
     },
 
     {
@@ -106,7 +106,7 @@ const journeyTimeline = [
     {
         type: "text",
         content:
-            "Even though I was enjoying digital art, I started doing animations too. I began to animate the characters I drew and I found happiness from that too. And most importantly my audiences liked the animations. So I didn't stop making animations. But the breakpoint wasn't the animations yet. One digital painting I did suddenly blew up on Instagram. I started getting more likes, engagement and followers. So i was really motivated to create more, I started getting more exposure, not only getting likes and follows, I started to make connections with people in the community. It was at this time I also stopped running from my responsibilities and started to draw backgrounds. But with good exposure came a villain called 'Perfectionism'. I became doubtful again. I was always asking myself 'Is this enough?'. But I knew I had to keep going.🌻"
+            "Even though I was enjoying digital art, I started doing animations too. I began to animate the characters I drew and I found happiness from that too. And most importantly my audiences liked the animations. So I didn't stop making animations. But the breakpoint wasn't the animations yet. One digital painting I did (Zephyr) suddenly blew up on Instagram. I started getting more likes, engagement and followers. So I was really motivated to create more, I started getting more exposure, not only getting likes and follows, I started to make connections with people in the community. It was at this time I also stopped running from my responsibilities and started to draw backgrounds. But with good exposure came a villain called 'Perfectionism'. I became doubtful again. I was always asking myself 'Is this enough?'. But I knew I had to keep going.🌻"
     },
 
     {
@@ -128,7 +128,7 @@ const journeyTimeline = [
     {
         type: "text",
         content:
-            "I was aware that if I want to go to the next level, I had to step out of my comfort zone. At some point I was thinking 'Am i doing this only for likes? Am I losing passion for art?'. Well, that is still unanswered. But I want to keep my passion alive as long as I'm alive.  I started making short animated series. And after a couple of series, 'Paper hearts' Went viral (Kinda). Even though it wasn't much views comparing to those huge views other artists were getting, I was happier than ever.  But even though the short stories got more responses, I continued making digital artworks. I wanted to improve the compositions and colors even more. Yeah, I still compare myself to other great artists, I judge my artworks by its likes, I still doubt about myself, even though I don't want to. Apart from my Computer Science career, I want to be known as an artist as well among people. Can I do it? As luffy said 'As long as I live, there are infinite chances.🪐"
+            "I was aware that if I want to go to the next level, I had to step out of my comfort zone. At some point I was thinking 'Am I doing this only for likes? Am I losing passion for art?'. Well, that is still unanswered. But I want to keep my passion alive as long as I'm alive.  I started making short animated series. And after a couple of series, 'Paper hearts' Went viral (Kinda). Even though it wasn't much views comparing to those huge views other artists were getting, I was happier than ever.  But even though the short stories got more responses, I continued making digital artworks. I wanted to improve the compositions and colors even more. Yeah, I still compare myself to other great artists, I judge my artworks by its likes, I still doubt about myself, even though I don't want to. Apart from my Computer Science career, I want to be known as an artist as well. Can I do it? As luffy said 'As long as I live, there are infinite chances'.🪐"
     },
 
     {
