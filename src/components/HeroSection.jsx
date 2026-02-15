9500303734a4d5acc9bcedc2defdaaafcc651c3d
Abhinandg2003 <abhinandg2003@gmail.com>
2026-02-15 15:09:50 +0530

    landing text upp

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -27,14 +27,14 @@ export const HeroSection = () => {
 
 <div className="container max-w-4xl text-left px-2 pr-25 sm:px-6 
                lg:ml-24 z-10 text-white
-               translate-y-[-3rem] sm:translate-y-[-1rem] md:translate-y-0">
+               translate-y-[-4rem] sm:translate-y-[-1rem] md:translate-y-0">
         <div className="space-y-1">
             <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
                 <span className="opacity-0 animate-fade-in-delay-1">Hi, I'm</span><br />
                 <span className=" opacity-0 animate-fade-in-delay-1"> Abhinand</span>
                 <span className=" ml-2 opacity-0 animate-fade-in-delay-1"> G</span>
                 <span className=" block opacity-0 animate-fade-in-delay-2"> also known as</span>
-                <span className=" block text-[#FA8655] opacity-0 animate-fade-in-delay-4 font-extrabold tracking-wide "> <img className="w-100 m:w-100 lg:w-100" src="Projects/logo 2.png" alt="aaaaa" /></span>
+                <span className=" block text-[#FA8655] opacity-0 animate-fade-in-delay-4 font-extrabold tracking-wide "> <img className="w-100 m:w-100 lg:w-100" src="Projects/logo.png" alt="aaaaa" /></span>
             </h1>
             <p className="text-lg  md:text-xl text-white max-2-2xl mx-auto opacity-0 animate-fade-in-delay-4">
                 Just an aspiring digital artist who loves turning <br /> emotions into little stories.
