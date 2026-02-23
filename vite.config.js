911a9c23836030a4b224ed1813f6591d36b20bd4
Abhinandg2003 <abhinandg2003@gmail.com>
2026-02-23 10:34:27 +0530

    redid

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'gh' ? '/Lazygraphite/' : '/',
+base: "/Lazygraphite/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
